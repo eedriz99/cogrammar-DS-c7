30f1fb9be04d6874fc41496672ec5deb117c1eca
eedriz99 <akinsolaidris1999@gmail.com>
2024-05-27 17:05:03 +0100

    Submission for task2

---
+++ conversion.py
@@ -0,0 +1,24 @@
+"""
+Type cast num1 from float to integer using the int() function
+Type cast num2 from integer to float using the float() function
+Type cast num3 from integer to string using the str() function
+Type cast string1 from string to integer using the int() function
+print all variable on different lines.
+"""
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# conversion
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+# Print
+print(num1)
+print(num2)
+print(num3)
+print(string1)
