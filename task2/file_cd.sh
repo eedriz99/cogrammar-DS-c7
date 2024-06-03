cda1ba1986f51cb7b2cf7cb942bc7ade4b525641
eedriz99 <akinsolaidris1999@gmail.com>
2024-06-03 13:06:27 +0100

    Submission for task2

---
+++ task2/file_cd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir first_directory
+mkdir second_directory
+mkdir third_directory
+
+cd second_directory
+
+mkdir first_subdirectory
+mkdir second_subdirectory
+mkdir third_subdirectory
+
+rm -rf first_subdirectory
+rm -rf second_subdirectory
---
+++ task2/hello_world.py
@@ -0,0 +1,15 @@
+# Input name
+# save name in the variable name
+# Output the value saved in the variable name
+# Input age
+# Change user input to the integer (int) datatype
+# Save user age to variable name
+# Output the variable age
+# Output the string "Hello World!"
+
+
+name = input('Enter your name: ')
+print(name)
+age = int(input('Enter your age: '))
+print(age)
+print('Hello World!')
